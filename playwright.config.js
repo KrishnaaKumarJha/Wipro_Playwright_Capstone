8314b1f110def449dff5f10359949d83263acc4c
KrishnaaKumarJha <krishnakrsindia@gmail.com>
2026-05-28 12:23:05 +0530

    Allure report Generation

--- playwright.config.js
+++ playwright.config.js
@@ -33,8 +33,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Use 2 parallel workers on CI to fully utilize the 2 CPU cores of the runner */
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
